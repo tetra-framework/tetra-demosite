569c47f04f428175245be15e82693d65a6ed0af6
Christian González <christian.gonzalez@nerdocs.at>
2026-01-12 09:58:06 +0100

    import views as module

--- demo/urls.py
+++ demo/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import home, examples
 
+from . import views
+
 
 urlpatterns = [
     path("", home, name="home"),
     path("examples/", examples, name="examples-home"),
     path("examples/<slug:slug>/", examples, name="examples"),
+    path("", views.home, name="home"),
+    path("examples/", views.examples, name="examples-home"),
+    path("examples/<slug:slug>/", views.examples, name="examples"),
 ]
