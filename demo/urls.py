4172b41010ae5a02bb7c0f3ff84c545a1097df56
Christian González <christian.gonzalez@nerdocs.at>
2026-01-12 10:01:31 +0100

    remove Router, Link, Redirect from top level tetra module
    it should be iported from tetra.router

--- demo/urls.py
+++ demo/urls.py
@@ -1,14 +1,11 @@
-from django.urls import path
-from .views import home, examples
+from django.urls import path, re_path
 
 from . import views
 
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("examples/", examples, name="examples-home"),
-    path("examples/<slug:slug>/", examples, name="examples"),
     path("", views.home, name="home"),
     path("examples/", views.examples, name="examples-home"),
     path("examples/<slug:slug>/", views.examples, name="examples"),
+    re_path("^app/.*$", views.app_view, name="app_view"),
 ]
