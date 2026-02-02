e648a52625d33664538553ed71a0f4c7f2afbafb
Christian González <christian.gonzalez@nerdocs.at>
2026-02-02 12:27:30 +0100

    register `notifications.news.headline` in channels_group_registry in DemoConfig

--- demo/apps.py
+++ demo/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class DemoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "demo"
+
+    def ready(self):
+        from tetra.registry import channels_group_registry
+
+        channels_group_registry.register("notifications.news.headline")
